7fa08ddaa6812f8532b4ff08b7c20ea61b35e0dd
chipo <pnapoli33@gmail.com>
2020-11-13 17:38:07 -0300

    video 50.0 - Add @EnableDiscoveryClient and properties.

--- src/main/java/com/binary_winters/photoapp/api/gateway/PhotoAppApiZuulApiGatewayApplication.java
+++ src/main/java/com/binary_winters/photoapp/api/gateway/PhotoAppApiZuulApiGatewayApplication.java
@@ -2,8 +2,12 @@ package com.binary_winters.photoapp.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class PhotoAppApiZuulApiGatewayApplication {
 
 	public static void main(String[] args) {
